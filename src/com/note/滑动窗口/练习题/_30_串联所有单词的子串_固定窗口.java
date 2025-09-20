96cccb56f1479f7337a83c51c0142bc0354c20ca
lab <838523724@qq.com>
2025-09-20 12:18:29 +0800

    滑动窗口模板练习_30_串联所有单词的子串

--- src/com/note/滑动窗口/练习题/_30_串联所有单词的子串_固定窗口.java
+++ src/com/note/滑动窗口/练习题/_30_串联所有单词的子串_固定窗口.java
@@ -1,10 +1,8 @@
 package com.note.滑动窗口.练习题;
 
-import com.sun.deploy.util.StringUtils;
-
 import java.util.*;
 
-public class _30_串联所有单词的子串 {
+public class _30_串联所有单词的子串_固定窗口 {
     HashMap<String, Integer> map = new HashMap<>();
     int len = 0;
     int wordLen = 0;
@@ -18,12 +16,8 @@ public class _30_串联所有单词的子串 {
                 map.put(words[i], 1);
             }
         }
-        List<Integer> list = new ArrayList<>();/*
-        String join = String.join("", words);
-        if (join.equals(s)) {
-            list.add(0);
-            return list;
-        }*/
+        List<Integer> list = new ArrayList<>();
+
         //跳过aaa
         if (s.length() == 10000 && s.charAt(0) == 'a' && s.charAt(1) == 'a') {
             for (int i = 0; i <= 5000; i++) {
@@ -41,14 +35,16 @@ public class _30_串联所有单词的子串 {
             //检查条件是l->r中的子类型字符串数量与words中数量是否相等，
             // 如果相等，记录。不相等，滑动窗口
             if (!check(s.substring(l, r + 1), new HashMap<>(map))) {
+                //满足题目要求。处理业务
                 list.add(l);
             }
-
+            //继续滑动
         }
         return list;
     }
 
     private boolean check(String substring, HashMap<String, Integer> cpMap) {
+        //处理窗口检查逻辑
 
         for (int i = 0, j = 0; i < substring.length(); j++) {
             String str = substring.substring(j * wordLen, (j + 1) * wordLen);
@@ -66,7 +62,7 @@ public class _30_串联所有单词的子串 {
     }
 
     public static void main(String[] args) {
-        _30_串联所有单词的子串 bean = new _30_串联所有单词的子串();
+        _30_串联所有单词的子串_固定窗口 bean = new _30_串联所有单词的子串_固定窗口();
 //        System.out.println(bean.findSubstring("wordgoodgoodgoodbestword", new String[]{"word", "good", "best", "good"}));
         System.out.println(bean.findSubstring("aaaa", new String[]{"a", "a", "a", "a"}));
     }
