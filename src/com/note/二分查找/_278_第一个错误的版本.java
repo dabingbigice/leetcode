b312af65aded5225fe3745f0bbf91fcf459c9965
liaobing8319 <838523724@qq.com>
2025-09-26 16:17:03 +0800

    二分查找_278_第一个错误的版本

--- src/com/note/二分查找/_278_第一个错误的版本.java
+++ src/com/note/二分查找/_278_第一个错误的版本.java
@@ -7,8 +7,8 @@ public class _278_第一个错误的版本 {
         while (l < r) {
             // int mid = (l-r)/2+r;
             int mid = ((l - r) >> 1) + r;
-            if (isBadVersion(mid)) r = mid;
-            else l = mid + 1;
+//            if (isBadVersion(mid)) r = mid;
+//            else l = mid + 1;
         }
         return l;
 
