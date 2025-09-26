f56529b812c8999c9a4890b7245740a3346610d3
liaobing8319 <838523724@qq.com>
2025-09-26 15:19:09 +0800

    二分查找_69_x的平方根

--- src/com/note/二分查找/_69_x的平方根.java
+++ src/com/note/二分查找/_69_x的平方根.java
@@ -5,7 +5,7 @@ public class _69_x的平方根 {
     public int mySqrt(int x) {
         int l = 0, r = x;
         while (l < r) {
-            //大于最小值的最大值
+            //小于目标值的最大值
             int mid =((l - r + 1)>>1) +r;
             if ((long) mid * mid <= x) l = mid ;
             else r = mid-1;
