d6b34d4683af3943861ce409e8c4c922ea681dcf
lab <838523724@qq.com>
2025-09-26 14:58:40 +0800

    二分查找_69_x的平方根

--- src/com/note/二分查找/_69_x的平方根.java
+++ src/com/note/二分查找/_69_x的平方根.java
@@ -5,9 +5,10 @@ public class _69_x的平方根 {
     public int mySqrt(int x) {
         int l = 0, r = x;
         while (l < r) {
-            int mid = (l + r + 1) / 2;
-            if ((long) mid * mid < x) l = mid + 1;
-            else r = mid;
+            //大于最小值的最大值
+            int mid =((l - r + 1)>>1) +r;
+            if ((long) mid * mid <= x) l = mid ;
+            else r = mid-1;
         }
         return l;
     }
