f3d4ae7d76fd8ff4e672f36c325cc6140e9cef5b
liaobing8319 <838523724@qq.com>
2025-09-26 16:16:45 +0800

    二分查找_278_第一个错误的版本

--- src/com/note/二分查找/_278_第一个错误的版本.java
+++ src/com/note/二分查找/_278_第一个错误的版本.java
@@ -1,4 +1,16 @@
 package com.note.二分查找;
 
 public class _278_第一个错误的版本 {
+    public int firstBadVersion(int n) {
+        int l = 0, r = n;
+        //找最大值最小
+        while (l < r) {
+            // int mid = (l-r)/2+r;
+            int mid = ((l - r) >> 1) + r;
+            if (isBadVersion(mid)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+
+    }
 }
