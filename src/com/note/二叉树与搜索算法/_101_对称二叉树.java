e926439c2e51179669dc0f0e292474fd0ee33ada
lab <838523724@qq.com>
2025-10-16 15:53:40 +0800

    二叉树_101_对称二叉树

---
+++ src/com/note/二叉树与搜索算法/_101_对称二叉树.java
@@ -0,0 +1,15 @@
+package com.note.二叉树与搜索算法;
+
+public class _101_对称二叉树 {
+    public boolean isSymmetric(TreeNode root) {
+        //转过来进行一次层次遍历
+        return dfs(root.left, root.right);
+    }
+
+    boolean dfs(TreeNode l, TreeNode r) {
+        if (l == null || r == null) return l == null && r == null;
+        return l.val == r.val && dfs(l.left, r.right) && dfs(l.right, r.left);
+    }
+
+
+}
--- src/com/note/二叉树与搜索算法/_226_翻转二叉树.java
+++ src/com/note/二叉树与搜索算法/_226_翻转二叉树.java
@@ -1,5 +1,8 @@
 package com.note.二叉树与搜索算法;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class _226_翻转二叉树 {
 
 
@@ -16,5 +19,26 @@ public class _226_翻转二叉树 {
         root.right = temp;
         dsf(root.left);
         dsf(root.right);
+    }
+
+    void bsf(TreeNode root) {
+        Deque<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.pollFirst();
+                TreeNode temp = treeNode.left;
+                treeNode.left = treeNode.right;
+                treeNode.right = temp;
+                if (treeNode.left != null) {
+                    queue.addLast(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.addLast(treeNode.right);
+                }
+            }
+
+        }
     }
 }
