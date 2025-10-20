d153043271d76fb9be2d4e87791d211e786fa395
lab <838523724@qq.com>
2025-10-20 16:43:54 +0800

    二叉树_257_二叉树的所有路径

---
+++ src/com/note/二叉树与搜索算法/_257_二叉树的所有路径.java
@@ -0,0 +1,31 @@
+package com.note.二叉树与搜索算法;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _257_二叉树的所有路径 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        dfs(root, new StringBuffer());
+        return list;
+    }
+
+    ArrayList list = new ArrayList<TreeNode>();
+
+    void dfs(TreeNode root, StringBuffer path) {
+        //1.结束条件
+        if (root == null) return;
+
+        //2.逻辑处理
+        //拼接
+        StringBuffer cur = new StringBuffer(path);
+        cur.append(root.val);
+        if (root.left == root.right) {
+            //叶子节点
+            list.add(cur.toString());
+            return;
+        }
+        cur.append("->");
+        dfs(root.left, cur);
+        dfs(root.right, cur);
+    }
+}
