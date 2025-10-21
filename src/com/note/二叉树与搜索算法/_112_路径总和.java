1187da17b1c706975586c853ce0cae491a670174
lab <838523724@qq.com>
2025-10-21 14:17:54 +0800

    二叉树_112_路径总和

--- src/com/note/二叉树与搜索算法/_112_路径总和.java
+++ src/com/note/二叉树与搜索算法/_112_路径总和.java
@@ -1,5 +1,8 @@
 package com.note.二叉树与搜索算法;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class _112_路径总和 {
     boolean flag = false;
 
@@ -20,5 +23,34 @@ public class _112_路径总和 {
         sumNumber += treeNode.val;
         dfs(treeNode.left, targetSum, sumNumber);
         dfs(treeNode.right, targetSum, sumNumber);
+    }
+
+
+    boolean dfs1(TreeNode root, int cursum) {
+        if (root == null) return false;
+        if (root.left == root.right) return cursum == root.val;
+        return dfs1(root.left, cursum - root.val) || dfs1(root.right, cursum - root.val);
+    }
+
+    boolean bfs(TreeNode treeNode, int targetSum) {
+        Deque<TreeNode> q = new LinkedList<>();
+        if (treeNode != null) q.add(treeNode);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.pollFirst();
+                //当寻找到满足条件的时候才停止搜索
+                if (node.left == node.right && node.val == targetSum) return true;
+                if (node.left != null) {
+                    node.left.val += node.val;
+                    q.addLast(node.left);
+                }
+                if (node.right != null) {
+                    node.right.val += node.val;
+                    q.addLast(node.right);
+                }
+            }
+        }
+        return false;
     }
 }
