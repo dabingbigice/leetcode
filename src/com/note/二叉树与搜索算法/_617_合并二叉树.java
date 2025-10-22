91f4513cc8a4f41d9a60f7853fb73dfbef3debf5
lab <838523724@qq.com>
2025-10-22 16:10:07 +0800

    二叉树_617_合并二叉树

---
+++ src/com/note/二叉树与搜索算法/_617_合并二叉树.java
@@ -0,0 +1,29 @@
+package com.note.二叉树与搜索算法;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class _617_合并二叉树 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
+        return null;
+    }
+
+    void bfs(TreeNode root1, TreeNode root2) {
+        Deque<TreeNode> q1 = new LinkedList<>();
+        Deque<TreeNode> q2 = new LinkedList<>();
+        q1.addFirst(root1);
+        q2.addFirst(root2);
+
+    }
+
+    TreeNode dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == root2) return null;
+        int v1 = root1 == null ? 0 : root1.val;
+        int v2 = root2 == null ? 0 : root2.val;
+        TreeNode node = new TreeNode(v1 + v2);
+        node.left = dfs(root1 == null ? null : root1.left, root2 == null ? null : root2.left);
+        node.right = dfs(root1 == null ? null : root1.right, root2 == null ? null : root2.right);
+        return node;
+    }
+}
