c2cfbe8aaaacae43a2e00d436810ed3729746fc4
lab <838523724@qq.com>
2025-10-22 16:38:16 +0800

    二叉树_235_二叉搜索树的最近公共祖先

---
+++ src/com/note/二叉树与搜索算法/_235_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,10 @@
+package com.note.二叉树与搜索算法;
+
+public class _235_二叉搜索树的最近公共祖先 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+}
--- src/com/note/二叉树与搜索算法/_617_合并二叉树.java
+++ src/com/note/二叉树与搜索算法/_617_合并二叉树.java
@@ -19,9 +19,11 @@ public class _617_合并二叉树 {
 
     TreeNode dfs(TreeNode root1, TreeNode root2) {
         if (root1 == root2) return null;
+        //其中一个不为Null,开始构造节点。
         int v1 = root1 == null ? 0 : root1.val;
         int v2 = root2 == null ? 0 : root2.val;
         TreeNode node = new TreeNode(v1 + v2);
+        //从递归最底层开始向上构造
         node.left = dfs(root1 == null ? null : root1.left, root2 == null ? null : root2.left);
         node.right = dfs(root1 == null ? null : root1.right, root2 == null ? null : root2.right);
         return node;
