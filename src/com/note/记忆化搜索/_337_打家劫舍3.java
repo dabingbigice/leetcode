b64c670c4199ac7e296d3cf06760896ac99d90f9
lab <838523724@qq.com>
2025-10-31 19:42:05 +0800

    记忆搜索_494_目标和

--- src/com/note/记忆化搜索/_337_打家劫舍3.java
+++ src/com/note/记忆化搜索/_337_打家劫舍3.java
@@ -15,6 +15,21 @@ public class _337_打家劫舍3 {
     int dfs(TreeNode root) {
         if (root == null) return 0;
         if (map.containsKey(root)) return map.get(root);
+        //偷和不偷的决定时机是先偷了，知道了结果再去取舍，偷大的那个。
+        //所以存在重复运算
+        /*
+                     A(3)
+                   /      \
+                B(2)     C(3)
+                  \        \
+                  D(3)     E(1)
+        * dfs(A)
+          → sel_A: 需计算 dfs(D) 和 dfs(E)   [第一次计算D、E]
+          → unsel_A: 需计算 dfs(B) 和 dfs(C)
+          → dfs(B): 内部计算 dfs(D)      [第二次计算D],不偷B偷下一层
+          → dfs(C): 内部计算 dfs(E)      [第二次计算E],不偷E偷下一层
+        * */
+
         //偷当前节点
         int sel = root.val;
         //偷下下层满足条件的节点
---
+++ src/com/note/记忆化搜索/_494_目标和.java
@@ -0,0 +1,24 @@
+package com.note.记忆化搜索;
+
+import com.sun.org.apache.xml.internal.security.Init;
+
+public class _494_目标和 {
+    int dp[];
+
+    public int findTargetSumWays(int[] nums, int target) {
+        dp = new int[nums.length];
+        return dfs(nums, 0, 0, target);
+    }
+
+
+    int dfs(int[] nums, int idx, int sum, int target) {
+        if (idx == nums.length) return target == sum ? 1 : 0;
+        int add = dfs(nums, idx + 1, sum + nums[idx], target);
+        int sub = dfs(nums, idx + 1, sum - nums[idx], target);
+        return add + sub;
+    }
+
+    public static void main(String[] args) {
+        new _494_目标和().dfs(new int[]{1, 1, 1, 1, 1}, 0, 0, 3);
+    }
+}
