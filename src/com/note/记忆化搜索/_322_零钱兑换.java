be61fce54a0bf57ccdfaa4a2c3e7a1fe642d7408
lab <838523724@qq.com>
2025-11-02 15:35:37 +0800

    记忆搜索_322_零钱兑换

--- src/com/note/记忆化搜索/_322_零钱兑换.java
+++ src/com/note/记忆化搜索/_322_零钱兑换.java
@@ -1,13 +1,24 @@
 package com.note.记忆化搜索;
 
+import java.util.Arrays;
+
 public class _322_零钱兑换 {
     public int coinChange(int[] coins, int amount) {
-
-        return 0;
+        dp = new int[amount+1];
+        Arrays.fill(dp, -1);
+        int ans = dfs(0,coins,amount);
+        return ans == 10010 ? -1 : ans;
     }
 
-    int dfs(int[] coins) {
+    int dp[];
 
-        return 0;
+    int dfs(int cur, int[] coins, int amount) {
+        if (cur >= amount) return cur == amount ? 0 : 10010;
+        if (dp[cur] != -1) return dp[cur];
+        int ans = 10010;
+        for (int i = 0; i < coins.length; i++) {
+            if(coins[i]<=amount-cur) ans = Math.min(dfs(cur + coins[i], coins, amount) + 1, ans);
+        }
+        return dp[cur] = ans;
     }
 }
