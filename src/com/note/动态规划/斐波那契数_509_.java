06a19ca54a6288ddf4182d2092262ace347d929c
lab273 <838523724@qq.com>
2025-11-04 15:25:10 +0800

    爬楼梯_70

--- src/com/note/动态规划/斐波那契数_509_.java
+++ src/com/note/动态规划/斐波那契数_509_.java
@@ -7,13 +7,13 @@ public class 斐波那契数_509_ {
 
     public int fib(int n) {
         if (n <= 1) return n;
-        dp = new int[n + 1];
+        dp = new int[n];
         dp[0] = dp[1] = 1;
-        for (int i = 2; i < n + 1; i++) {
+        for (int i = 2; i < n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         System.out.println(Arrays.toString(dp));
-        return dp[dp.length - 2];
+        return dp[dp.length - 1];
     }
 
     public static void main(String[] args) {
