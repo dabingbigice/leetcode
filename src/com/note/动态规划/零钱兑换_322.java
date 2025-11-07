5bebfca80c55c13427bd91277d7f4cfa703f3a35
lab <838523724@qq.com>
2025-11-07 22:29:17 +0800

    记忆搜索_343_整数拆分

--- src/com/note/动态规划/零钱兑换_322.java
+++ src/com/note/动态规划/零钱兑换_322.java
@@ -15,9 +15,10 @@ public class 零钱兑换_322 {
         // 动态规划过程
         for (int i = 0; i < coins.length; i++) {         // 遍历物品（硬币）
             for (int j = coins[i]; j <= amount; j++) {  // 遍历背包容量（金额），正序以实现完全背包
-                // 确保j - coins[i]是有效状态（即可达状态）
+                // 确保j - coins[i]是有效状态（即可达状态）,
+                // 没装满的都是最大值，取不到
                 if (dp[j - coins[i]] != Integer.MAX_VALUE) {
-                    // 状态转移：核心逻辑，使用当前硬币coins[i],没装满的都是最大值，取不到
+                    // 状态转移：核心逻辑，使用当前硬币coins[i],
                     dp[j] = Math.min(dp[j], 1 + dp[j - coins[i]]); // 修正了这里，使用coins[i]
                 }
             }
