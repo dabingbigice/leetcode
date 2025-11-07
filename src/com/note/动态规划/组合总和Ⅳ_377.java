3850d3f52c20cc8f15880e0bb56215dca8add212
lab273 <838523724@qq.com>
2025-11-07 15:15:13 +0800

    组合总和Ⅳ_377

---
+++ src/com/note/动态规划/组合总和Ⅳ_377.java
@@ -0,0 +1,22 @@
+package com.note.动态规划;
+
+import java.util.Arrays;
+
+public class 组合总和Ⅳ_377 {
+    public int combinationSum4(int[] nums, int target) {
+        //组合数。方案个数
+        //定义dp[j]表示满足target为j的组合总数
+        int[] dp = new int[target + 1];
+        //初始化
+        dp[0] = 1;
+        //递推式 dp[j] = dp[j]+dp[j-weight[i]]
+        for (int i = 1; i <= target; i++) {//背包
+            for (int j = 0; j < nums.length; j++) {//物品
+                //物品进行组合
+                if (nums[j] <= i) dp[i] = dp[i] + dp[i - nums[j]];
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[target];
+    }
+}
