cca03a1f963076b46efa1f545efd09f1bdb8c608
lab273 <838523724@qq.com>
2025-11-10 17:12:11 +0800

    买卖股票的最佳时机III_123

--- src/com/代码随想录/动态规划/买卖股票的最佳时机II_122.java
+++ src/com/代码随想录/动态规划/买卖股票的最佳时机II_122.java
@@ -10,8 +10,7 @@ public class 买卖股票的最佳时机II_122 {
         int dp[] = new int[prices.length];
         dp[1] = Math.max(prices[1] - prices[0], 0);
         for (int i = 2; i < prices.length; i++) {
-            if (prices[i] - prices[i - 1] > 0) dp[i] = prices[i] - prices[i - 1] + dp[i - 1];
-            else dp[i] = dp[i - 1];
+            dp[i] = Math.max(dp[i - 1], prices[i] - prices[i - 1] + dp[i - 1]);
         }
         System.out.println(Arrays.toString(dp));
         return dp[prices.length - 1];
