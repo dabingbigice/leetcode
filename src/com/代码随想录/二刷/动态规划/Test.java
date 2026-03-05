b299bd5f6f7eb1f22790fedacb7b7f1b505ead44
lab <838523724@qq.com>
2026-03-05 23:51:10 +0800

    动态规划

--- src/com/代码随想录/二刷/动态规划/Test.java
+++ src/com/代码随想录/二刷/动态规划/Test.java
@@ -453,4 +453,14 @@ public class Test {
         return dp[word1.length()][word2.length()];
     }
 
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length][nums2.length];
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[i]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[nums1.length-1][nums2.length-1];
+    }
 }
